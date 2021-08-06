00eaf6b5abeabafad01da4aed56c9ddf0e6880a0
Mathias Nikkel <mathias.nikkel@ambevtech.com.br>
2021-08-06 14:20:40 -0300

    feat: working monorepo

---
+++ packages/app1/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Button } from '@cmm/components'
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
